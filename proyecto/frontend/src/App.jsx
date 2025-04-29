c5e8bfeb73c65d8f73ce5c59d143d2b01e98208b
miguel <miguelangelroigancio@gmail.com>
2025-04-29 16:56:13 +0200

    Disseny Inici de sessió

--- proyecto/frontend/src/App.jsx
+++ proyecto/frontend/src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Container fluid className='fons'>
+        <Row className="d-flex align-items-center justify-content-center vh-100">
+          <Col>
+            <div className='d-flex justify-content-center'>
+              <Card className='rounded-4 formulari' style={{ width: '25rem' }}>
+                <Card.Body>
+                  <Card.Title className='text-center fs-2'>FitMatch</Card.Title>
+                  <div>
+                    <Card.Text>
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <div className='d-flex justify-content-center'>
+                        <Button className='boto' variant="primary" type="submit">
+                          Inicia Sessió
+                        </Button>
+                      </div>     
+                      <a href="#" className='d-flex justify-content-center pt-2'>No tens compte? Registrat</a> 
+                      </Form>
+                    </Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </Row>
+      </Container> 
     </>
   )
 }
