9e21a57f39b48d2ef5a1a23aea9a5db1e97dcc2e
miguel <miguelangelroigancio@gmail.com>
2025-05-06 16:46:04 +0200

    modal registrarse

--- proyecto/frontend/src/components/lateral.jsx
+++ proyecto/frontend/src/components/lateral.jsx
@@ -1,6 +1,8 @@
 // src/components/Header.jsx
 import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import { BsSearch, BsPlusCircle, BsClockHistory, BsPerson, BsBoxArrowRight } from 'react-icons/bs';
+
 import { Link } from 'react-router-dom'; // Para navegación interna
 import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -9,20 +11,30 @@ const Header = () => {
      
         <Col xs="2" className='border border-black colorPrincipal p-0'>
         <ListGroup className='menuLateral'>
-          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista '>
-            Busca partit
+          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista d-flex align-items-center'>
+            <a href="#" className='colorText text-decoration-none'>
+              <BsSearch className='me-2' />Busca partit
+            </a>      
           </ListGroup.Item>
-          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista'> 
-            Crear Partit
+          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista py-0 d-flex align-items-center '> 
+            <a href="#" className='colorText text-decoration-none'>
+              <BsPlusCircle className='me-2' />Crear Partit
+            </a>        
           </ListGroup.Item>
-          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista'>
-            Historial
+          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista py-0 d-flex align-items-center'>
+            <a href="#" className='colorText text-decoration-none'>
+              <BsClockHistory className='me-2' />Historial
+            </a>        
           </ListGroup.Item>
-          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista'>
-            Perfil
+          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista py-0 d-flex align-items-center'>
+            <a href="#" className='colorText text-decoration-none'>
+              <BsPerson className='me-2' />Perfil
+            </a>
           </ListGroup.Item>
-          <ListGroup.Item className='colorPrincipal colorText border border-0 border-bottom elementoLista'>
-            Tanca Sessió
+          <ListGroup.Item className='colorPrincipal border border-0 border-bottom elementoLista py-0 d-flex align-items-center'>
+            <a href="#" className='colorText text-decoration-none'>
+              <BsBoxArrowRight className='me-2' />Tanca Sessió
+            </a>        
           </ListGroup.Item>
         </ListGroup>
         </Col>
--- proyecto/frontend/src/views/app/login.jsx
+++ proyecto/frontend/src/views/app/login.jsx
@@ -6,25 +6,26 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import Lateral from '../../components/lateral'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
     <>
-    
     <Container fluid className='fonsIniciSessio'>
-        <Row className="d-flex align-items-center justify-content-center vh-100">
-          <Lateral />
-          <Col xs="10">
-            <div className='d-flex justify-content-center'>
-              <Card className='rounded-4 colorPrincipal colorText' style={{ width: '25rem' }}>
-                <Card.Body>
-                  <Card.Title className='text-center fs-2'>Inicia Sessió</Card.Title>
-                  <div>
-                    <Card.Text>
+      <Row className="d-flex align-items-center justify-content-center vh-100">
+        <Col>
+          <div className='d-flex justify-content-center'>
+            <Card className='rounded-4 colorPrincipal colorText' style={{ width: '25rem' }}>
+              <Card.Body>
+                <Card.Title className='text-center fs-2'>Inicia Sessió</Card.Title>
+                <div>
+                  <Card.Text>
                     <Form>
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
@@ -40,18 +41,61 @@ function App() {
                           Inicia Sessió
                         </Button>
                       </div>     
-                      <a href="#" className='d-flex justify-content-center pt-2 colorText'>No tens compte? Registrat</a> 
-                      </Form>
-                    </Card.Text>
-                  </div>
-                </Card.Body>
-              </Card>
-            </div>
-          </Col>
-        </Row>
-      </Container> 
+                      <a onClick={handleOpenModal} className='d-flex justify-content-center pt-2 colorText' style={{ cursor: 'pointer' }}>
+                        No tens compte? Registrat
+                      </a> 
+                    </Form>
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+        </Col>
+      </Row>
+    </Container> 
+
+    {/* Modal de Registro */}
+    <Modal show={showModal} onHide={handleCloseModal} centered>
+      <Modal.Header closeButton className='colorPrincipal colorText'>
+        <Modal.Title className='fs-2 w-100 text-center'>Registra't</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='colorPrincipal colorText'>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Nom</Form.Label>
+            <Form.Control type="text" placeholder="Introdueix el teu nom" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Edat</Form.Label>
+            <Form.Control type="number" placeholder="Introdueix la teva edat" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmailRegister">
+            <Form.Label>Correu Electronic</Form.Label>
+            <Form.Control type="email" placeholder="Introdueix el teu email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPasswordRegister">
+            <Form.Label>Contrasenya</Form.Label>
+            <Form.Control type="password" placeholder="Contrasenya" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label>Confirma la contrasenya</Form.Label>
+            <Form.Control type="password" placeholder="Confirma la contrasenya" />
+          </Form.Group>
+
+          <div className='d-flex justify-content-center'>
+            <Button variant="primary" type="submit" className='boto'>
+              Registrar-se
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
     </>
   )
 }
 
-export default App
+export default App;
